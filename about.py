422e52b8bf7fc7204600abe7d7d676167b7029ac
wuziyue840 <3931968261@qq.com>
2026-09-26 20:17:20 +0800

    ci:修改配置参数

--- about.py
+++ about.py
@@ -16,7 +16,7 @@ import webbrowser
 from tkinter import ttk, messagebox
 
 APP_NAME = "开发者调试工具 / devbugtools"
-APP_VERSION = "v1.0.0"
+APP_VERSION = "v1.1.0"
 DEVELOPER = "倾听风雨 / wuziyue840"
 LICENSE_NAME = "MIT"
 
